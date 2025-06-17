43b3d4491bc221ba0cc52ea4f0ea7c587df26336
pchanu023 <pchanu023@gmail.com>
2025-06-17 14:27:53 +0700

    feat: v-card-title

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-subtitle class="p-10">
+        ระหว่างดําเนินการ
+      </v-card-subtitle>
+      <v-card-title>
+        ขอมูลทั่วไปของคดี
+      </v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
